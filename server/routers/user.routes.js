e65fa2517997b0dbeb27021b5c739fe425dc927a
Encoder012 <49637067+Encoder012@users.noreply.github.com>
2025-08-31 17:37:27 +0530

    fixes: check username route change

--- server/routers/user.routes.js
+++ server/routers/user.routes.js
@@ -1,17 +1,17 @@
-const {register} = require("../controllers/user.controller");
-const {login} = require("../controllers/user.controller");
-const {logout} = require("../controllers/user.controller");
+const { register } = require("../controllers/user.controller");
+const { login } = require("../controllers/user.controller");
+const { logout } = require("../controllers/user.controller");
 const verifyjwt = require("../middlewares/auth.middleware");
-const {generateNewAccessToken} = require("../controllers/user.controller");
-const {change_password} = require("../controllers/user.controller");
-const {checkNameUniqueness} = require("../controllers/user.controller");
-const {Router} = require("express");     
+const { generateNewAccessToken } = require("../controllers/user.controller");
+const { change_password } = require("../controllers/user.controller");
+const { checkNameUniqueness } = require("../controllers/user.controller");
+const { Router } = require("express");
 const router = Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/logout").post(verifyjwt,logout);
+router.route("/logout").post(verifyjwt, logout);
 router.route("/generateNewAccessToken").post(generateNewAccessToken);
 router.route("/changePassword").post(change_password);
-router.route("/checkUsername").post(checkNameUniqueness);
+router.route("/check-username").get(checkNameUniqueness);
 module.exports = router;
 
